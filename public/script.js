35bcb6c77ea188b9ef9330465bcacb6bd5b03638
bhapris-glitch <laybokalandd@gmail.com>
2026-04-18 08:58:20 -0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,3 +7,19 @@ function toggleMenu(){
     nav.style.display = "flex";
   }
 }
+
+// TESTIMONIAL AUTO SLIDER
+let index = 0;
+
+function showTestimonials(){
+  const items = document.querySelectorAll(".testimonial");
+
+  items.forEach(el => el.classList.remove("active"));
+
+  index++;
+  if(index >= items.length) index = 0;
+
+  items[index].classList.add("active");
+}
+
+setInterval(showTestimonials, 3000);
