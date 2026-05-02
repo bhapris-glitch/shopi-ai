9688ed6e42047c340efef10f47df2edd507e948a
bhapris-glitch <laybokalandd@gmail.com>
2026-05-02 21:16:31 +0530

    Refactor server.js to improve structure and readability

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const fetch = require("node-fetch");
+await fetch(...);
 const crypto = require("crypto");
 const bodyParser = require("body-parser");
 
