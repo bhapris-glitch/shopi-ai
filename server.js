e4de7223e118b2a83485ef85e35528f8050f32c2
bhapris-glitch <laybokalandd@gmail.com>
2026-06-06 20:35:33 +0530

    Refactor server.js for improved structure and security

--- server.js
+++ server.js
@@ -128,7 +128,11 @@ app.use(compression());
 app.use(morgan("tiny"));
 
 app.use(cors({
-  origin: "*",
+  origin: [
+  "https://layboka.com",
+  "https://www.layboka.com",
+  "https://app.layboka.com"
+]
   credentials: true
 }));
 
